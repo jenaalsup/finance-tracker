7975cc060dfc076e0a5aed9ef4f34e7984251188
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-08-14 14:32:28 -0700

    Display user/friend profile and tracking options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -8,7 +9,12 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
- def search
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
+  def search
     if params[:friend].present?
       @friends = User.search(params[:friend])
       @friends = current_user.except_current_user(@friends)
